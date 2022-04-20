b03897439920d0aaa611dc8acb620757de59ee46
erfan <e.nabavi78@gmail.com>
2022-04-20 18:39:56 +0430

    vercel v1

--- pages/paths.js
+++ pages/paths.js
@@ -1,4 +1,4 @@
-import { useRouter } from "next/router";
+/*import { useRouter } from "next/router";
 export default function Post({ post }) {
   console.log(post);
   const router = useRouter();
@@ -27,3 +27,4 @@ export async function getStaticProps(context) {
   console.log(post);
   return { props: { post } };
 }
+*/
