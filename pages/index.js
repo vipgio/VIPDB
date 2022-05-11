0971ba1f2bfb7a7ca74134862a0c525cbd321258
erfan <e.nabavi78@gmail.com>
2022-05-12 02:20:34 +0430

    trends no api

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,8 @@
 import Meta from "../components/Meta";
 import TitleSlider from "../HOC/TitleSlider";
 import { server } from "../HOC/config";
-import { useEffect } from "react";
-const axios = require("axios").default;
 
 export default function Home({ trends }) {
-	console.log(trends);
-	useEffect(async () => {
-		const options = {
-			method: "GET",
-			url: `${server}/api/trending`,
-		};
-		try {
-			const data = await axios.request(options);
-			console.log(data.data);
-		} catch (error) {
-			console.log(error);
-		}
-	}, []);
 	return (
 		<div className='p-1'>
 			<Meta title='Home | VIPDB' />
@@ -35,8 +20,8 @@ export default function Home({ trends }) {
 
 export async function getStaticProps() {
 	const res = await fetch(
-		// `https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_KEY}`
-		`${server}/api/trending`
+		`https://api.themoviedb.org/3/trending/all/day?api_key=${process.env.TMDB_KEY}`
+		// `${server}/api/trending`
 	);
 	const trends = await res.json();
 
